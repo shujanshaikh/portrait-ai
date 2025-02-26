632816342313dba539db5956238aa584308e0696
shujanshaikh <shaikhshujan@gmail.com>
2025-02-26 14:40:18 +0530

    init

--- apps/backend/models/FalAi.ts
+++ apps/backend/models/FalAi.ts
@@ -9,29 +9,29 @@ export class FalModel extends BaseModel {
 
 
     public async generateImages(prompt: string, tensorPath: string) {
-        // const { request_id, response_url } = await fal.queue.submit("fal-ai/flux-lora", {
-        //     input: {
-        //         prompt: prompt,
-        //         loras: [{
-        //             path: tensorPath,
-        //             scale: 1.0,
-        //         }]
-        //     },
-        //     webhookUrl: `${process.env.WEB_BASE_URL}/fal-ai/webhook/image`,
-        // });
+        const { request_id, response_url } = await fal.queue.submit("fal-ai/flux-lora", {
+            input: {
+                prompt: prompt,
+                loras: [{
+                    path: tensorPath,
+                    scale: 1.0,
+                }]
+            },
+            webhookUrl: `${process.env.WEB_BASE_URL}/fal-ai/webhook/image`,
+        });
 
         return { request_id : "", response_url : "" }
     }
 
 
     public async trainingModel(zipUrl: string, tensorWord: string) {
-        // const { request_id, response_url } = await fal.queue.submit("fal-ai/flux-lora-fast-training", {
-        //     input: {
-        //         images_data_url: zipUrl,
-        //         trigger_word: tensorWord
-        //     },
-        //     webhookUrl: `${process.env.WEB_BASE_URL}/fal-ai/webhook/train`,
-        // });
+        const { request_id, response_url } = await fal.queue.submit("fal-ai/flux-lora-fast-training", {
+            input: {
+                images_data_url: zipUrl,
+                trigger_word: tensorWord
+            },
+            webhookUrl: `${process.env.WEB_BASE_URL}/fal-ai/webhook/train`,
+        });
         return { request_id : "", response_url : "" }
     }
 
